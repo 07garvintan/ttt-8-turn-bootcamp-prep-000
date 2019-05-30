119e70d9640b3bbc662db13d0210a5b8c1af8106
garvin tan <garvintan07@gmail.com>
2019-05-30 05:30:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,8 +39,9 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index) == true 
     move(board,index,value="X")
+    display_board(board)
   else
      turn(board)
   end
-     display_board(board)
+     
   end
